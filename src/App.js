1710b69e5a294cd61e3e76324a6c440a97d868bc
perceptron007 <>
2022-03-03 13:00:35 +0000

    Added validations by using useFormik hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/form-control";
 import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
 import { Heading, VStack } from "@chakra-ui/layout"
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function App() {
 
@@ -11,7 +12,10 @@ function App() {
       username: "",
       password: "",
     },
-
+    validationSchema: Yup.object({
+      username: Yup.string().required("Username is required").min(6, "Should be atleast 6 characters"),
+      password: Yup.string().required("Password is required").min(6, "Should be atleast 6 characters")
+    }),
     onSubmit: (values, actions) => {
       alert(JSON.stringify(values, null, 2))
       actions.resetForm();
@@ -29,21 +33,25 @@ function App() {
         
       <Heading>Sign Up</Heading>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.username && formik.touched.username}>
         <FormLabel>UserName</FormLabel>
         <Input name="username"
           onChange={formik.handleChange}
           value={formik.values.username}
+          onBlur={formik.handleBlur}
           placeholder="Enter Username"/>
+        <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.password && formik.touched.password}>
         <FormLabel>Password</FormLabel>
         <Input name="password" 
           type="password"
           onChange={formik.handleChange}
           value={formik.values.password}
+          onBlur={formik.handleBlur}
           placeholder="Enter Password"/>
+        <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
       </FormControl>
 
       <Button type="submit" variant="outline" colorScheme="teal">
